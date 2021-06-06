2c941df2eb5545b7f8d932df93ac0732ce33dca4
Ivonne Fernandez <ivonne.fernandezch@gmail.com>
2021-06-05 21:35:19 -0300

    Refactor

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var reader *bufio.Reader
+
 func crearUsuario() {
 	fmt.Println("Usuario creado exitosamente!")
 }
@@ -22,38 +24,48 @@ func eliminarUsuario() {
 	fmt.Println("Usuario creado exitosamente!")
 }
 
-func main() {
+func readLine() string {
+	if option, err := reader.ReadString('\n'); err != nil {
+		panic("No es posible obtener el valor!")
+	} else {
+		return strings.TrimSpace(option) //eliminamos los espacios si no, no funciona
+		//option = strings.TrimSuffix(option, "\n") //eliminamos el salto de linea
+	}
 
-	var reader *bufio.Reader
+}
+
+func main() {
 	var option string
 
 	reader = bufio.NewReader(os.Stdin)
 
-	fmt.Println("A) Crear")
-	fmt.Println("B) Listar")
-	fmt.Println("C) Actualizar")
-	fmt.Println("D) Eliminar")
+	for {
+		fmt.Println("A) Crear")
+		fmt.Println("B) Listar")
+		fmt.Println("C) Actualizar")
+		fmt.Println("D) Eliminar")
 
-	fmt.Print("Ingresa una opcion válida: ")
-	option, err := reader.ReadString('\n')
+		fmt.Print("Ingresa una opcion válida: ")
+		option = readLine()
+		if option == "quit" || option == "q" {
+			break
+		}
 
-	if err != nil {
-		panic("No es posible obtener el valor")
+		//fmt.Println(option)
+
+		switch option {
+		case "a", "crear":
+			crearUsuario()
+		case "b", "listar":
+			listarUsuarios()
+		case "c", "actualizar":
+			actualizarUsuarios()
+		case "d", "eliminar":
+			eliminarUsuario()
+		default:
+			fmt.Println("Opcion no valida")
+		}
+
 	}
-	//option = strings.TrimSuffix(option, "\n") //eliminamos el salto de linea
-	option = strings.TrimSpace(option) //eliminamos los espacios si no, no funciona
-	//fmt.Println(option)
 
-	switch option {
-	case "a", "crear":
-		crearUsuario()
-	case "b", "listar":
-		listarUsuarios()
-	case "c", "actualizar":
-		actualizarUsuarios()
-	case "d", "eliminar":
-		eliminarUsuario()
-	default:
-		fmt.Println("Opcion no valida")
-	}
 }
